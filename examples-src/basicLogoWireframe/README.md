871513b3503eeca5628ac743dd888a00d51b71b4
pissang <bm2736892@gmail.com>
2018-01-12 00:38:48 +0800

    Add logo wireframe example

---
+++ examples-src/basicLogoWireframe/README.md
@@ -0,0 +1,6 @@
+---
+title: ClayGL Logo with Wireframe
+category: basic
+---
+
+This example demonstrates how to draw wireframe with barycentric coordinates.
---
+++ examples-src/basicLogoWireframe/basicLogoWireframe.js
@@ -0,0 +1,45 @@
+var app = clay.application.create('#viewport', {
+
+    graphic: {
+        shadow: true,
+        linear: true,
+        tonemapping: true
+    },
+
+    init: function (app) {
+        // Create camera
+        this._camera = app.createCamera([0, 2, 15], [0, 2, 0]);
+
+        // Create light
+        app.createDirectionalLight([-1, -3, -1]);
+
+        this._orbitControl = new clay.plugin.OrbitControl({
+            target: this._camera,
+            domElement: app.container
+            // autoRotate: true
+        });
+
+        app.createAmbientCubemapLight('../assets/textures/hdr/Grand_Canyon_C.hdr', 0.7, 0.3);
+
+        // Load boombox model. return a load promise to make sure the look will be start after model loaded.
+        return app.loadModel('../assets/models/logo/logo.gltf').then((function (result) {
+            this._logoRoot = result.rootNode;
+
+            result.materials.forEach(function (material) {
+                material.set('roughness', 0.3);
+
+                // Line width of wireframe
+                material.set('lineWidth', 1);
+            });
+
+            result.meshes.forEach(function (mesh) {
+                // ClayGL use barycentric coords to draw wireframe.
+                mesh.geometry.generateBarycentric();
+            });
+        }).bind(this));
+    },
+
+    loop: function (app) {
+        this._orbitControl.update(app.frameTime);
+    }
+});
