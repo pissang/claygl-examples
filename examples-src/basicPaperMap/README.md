1cc7ea3bfa0b19da79e9c98ca6c17015cfe1d9cb
pissang <bm2736892@gmail.com>
2018-01-31 23:57:46 +0800

    Add basicPaperMap example

--- examples-src/basicPaperMap/README.md
+++ examples-src/basicPaperMap/README.md
@@ -1,5 +1,5 @@
 ---
 title: Dragging 3D Map
-category: complex
+category: basic
 extraScripts: https://cdn.jsdelivr.net/npm/maptalks@0.37.0/dist/maptalks.min.js
 ---
--- examples-src/basicPaperMap/basicPaperMap.js
+++ examples-src/basicPaperMap/basicPaperMap.js
@@ -1,4 +1,4 @@
-function createMap() {
+function createMap(app) {
     var canvas = document.createElement('canvas');
     canvas.width = 2048;
     canvas.height = 2048;
@@ -16,8 +16,8 @@ function createMap() {
         })
     });
 
-    canvas.style.cssText = 'position:absolute;right:5px;bottom:5px;width:256px;height:256px;';
-    document.body.appendChild(canvas);
+    canvas.style.cssText = 'position:absolute;right:5px;bottom:5px;width:256px;height:256px;border:2px solid #fff';
+    app.container.appendChild(canvas);
 
     return map;
 }
@@ -34,7 +34,7 @@ clay.application.create('#viewport', {
 
     init: function (app) {
 
-        var map = createMap();
+        var map = createMap(app);
 
         var diffuseMap = new clay.Texture2D({
             dynamic: true,
@@ -42,33 +42,48 @@ clay.application.create('#viewport', {
             image: map.getContainer()
         });
 
-        map.on('baselayerload', function () {
-            // diffuseMap.dynamic = false;
-        });
+        var planes = [];
+        for (var i = 0; i < 10; i++) {
+            var plane = app.createPlane({
+                diffuseMap: diffuseMap
+            });
+            planes.push(plane);
+            plane.rotation.rotateX(-Math.PI / 2);
+            plane.position.set(0, (i - 5) / 10 - 1, 0);
 
-        var cube = app.createCube();
-        this._cube = cube;
-
-        var plane = app.createPlane({
-            diffuseMap: diffuseMap
-        });
-        plane.rotation.rotateX(-Math.PI / 2);
-        plane.position.y = 0.101;
-        this._plane = plane;
-
-        cube.scale.set(1, 0.1, 1);
+            var y = plane.position.y + i * i * 0.04;
+            app.timeline.animate(plane.position, { loop: true })
+                .then(1000 - i * 100, {    // Delay
+                    y: plane.position.y
+                })
+                .then(1000, { // Up
+                    y: y
+                }, 'cubicOut')
+                .then(i * 100, { // Wait
+                    y: y
+                })
+                .then(i * 100, { // Delay
+                    y: y
+                })
+                .then(1000, {    // Drop
+                    y: plane.position.y
+                }, 'cubicOut')
+                .then(1000 - i * 100, {
+                    y: plane.position.y  // Wait
+                })
+                .start();
+        }
 
 
         var camera = app.createCamera([4, 4 * Math.sqrt(2), 4], [0, 0, 0], 'orthographic', [4, 4, 20]);
 
-        app.createDirectionalLight([-1, -2, -1], '#fff', 1.2);
+        var light = app.createDirectionalLight([-1, -2, -0.5], '#fff', 1);
+        light.shadowResolution = 1024;
         app.createAmbientLight('#fff', 0.1);
 
         app.container.style.background = 'linear-gradient(rgb(218, 210, 153), rgb(176, 218, 185))';
     },
 
     loop: function (app) {
-        // this._plane.material.set('uvRepeat', [0.5, 0.5]);
-        // this._plane.material.set('uvOffset', [app.elapsedTime * 5e-5, app.elapsedTime * 5e-5]);
     }
 });
