2d627f4328e0299fb2a4aefb22dc4bc750331e28
pissang <bm2736892@gmail.com>
2018-03-12 21:28:21 +0800

    Add advanced renderer demo

---
+++ examples-src/advancedRenderer/README.md
@@ -0,0 +1,9 @@
+---
+title: Monument with Advanced Renderer
+category: advanced
+scripts: ../lib/claygl-advanced-renderer.js
+---
+
+Use [ClayGL Advanced Renderer](https://github.com/pissang/claygl-advanced-renderer) for better rendering quality.
+
+Models are from [Magica Voxel](https://github.com/ephtracy/voxel-model/tree/master/vox/monument) and converted with [Clay Viewer](https://github.com/pissang/clay-viewer)
---
+++ examples-src/advancedRenderer/advancedRenderer.js
@@ -0,0 +1,52 @@
+var app = clay.application.create('#viewport', {
+
+    autoRender: false,
+
+    init: function (app) {
+
+        var adr = this._advancedRenderer = new ClayAdvancedRenderer(app.renderer, app.scene, app.timeline, {
+            shadow: {
+                enable: true
+            },
+            postEffect: {
+                bloom: {
+                    enable: false
+                },
+                screenSpaceAmbientOcclusion: {
+                    enable: true,
+                    radius: 2,
+                    intensity: 1.2
+                }
+            }
+        });
+
+        // Create camera
+        this._camera = app.createCamera([-10, 15, 20], [0, 5, 0]);
+
+        this._control = new clay.plugin.OrbitControl({
+            target: this._camera,
+            domElement: app.container,
+            timeline: app.timeline
+        });
+
+        // Only re-render when the control is updated.
+        this._control.on('update', function () {
+            adr.render();
+        }, this);
+
+        // Load model.
+        app.loadModel('../assets/models/monument/monu16.glb').then(function (result) {
+            result.rootNode.scale.set(0.1, 0.1, 0.1);
+            adr.render();
+        });
+
+        app.createAmbientCubemapLight('../assets/textures/hdr/pisa.hdr', 0.5, 0.5).then(function (result) {
+            adr.render();
+        });
+
+        // Create lights
+        app.createDirectionalLight([-1, -1, -1], '#fff', 2);
+    },
+
+    loop: function (app) {}
+});
